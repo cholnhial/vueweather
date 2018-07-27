e8085f35703943e533a37793216c587de03ec2c1
Chol Nhial <chol.nhial.chol@gmail.com>
2018-07-28 08:30:55 +1000

    changes to date

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -1,23 +1,55 @@
 <template>
-  <div class="container d-flex justify-content-center h-100">
-    <div class="weather rounded justify-content-center align-self-baseline mt-5">
+  <div class="container h-100">
+    <div class="d-flex justify-content-center ">
+      <div class="weather  justify-content-center align-self-baseline mt-5">
         <div class="main-weather">
           <span class="">
              <i :class="'main-weather-icon ' + weathercode"></i>
           <span class="temp">{{ weather.temp }}&deg;</span>
           </span>
-          <p class="text-center  display-medium">{{ weather.description }}</p>
+          <p class="text-center weather-description display-medium">{{ weather.description }}</p>
         </div>
+      </div>
+    </div>
+
+    <div class="d-flex justify-content-center ">
+      <div class="col-3">
+        <div class="weather  justify-content-center align-self-baseline mt-5">
+          <div class="main-weather-mini">
+
+          </div>
+      </div>
+      </div>
+      <div class="col-3">
+        <div class="weather  justify-content-center align-self-baseline mt-5">
+          <div class="main-weather-mini">
+
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="weather  justify-content-center align-self-baseline mt-5">
+          <div class="main-weather-mini">
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <div class="weather  justify-content-center align-self-baseline mt-5">
+          <div class="main-weather-mini">
+
+          </div>
+        </div>
+      </div>
     </div>
   </div>
-</template>const name = this.$route.query.name;
+</template>
 
 <script>
 import 'weather-icons/css/weather-icons.css'
 
 import {WeatherAPI} from '../WeatherAPI'
 
-const weatherApi = new WeatherAPI('a45ae09cab0dc5983275de1c4e64dcf0')
+let weatherApi = new WeatherAPI('46bbcf85e9a2e83913ce8271ffc4a79d')
 
 export default {
   name: 'Weather',
@@ -36,9 +68,14 @@ export default {
       }
 
       if (data) {
-        this.weather = data;
-        this.weathercode = weatherApi.getWeatherIcon(data.weathercode);
+        this.weather = data
+        this.weathercode = weatherApi.getWeatherIcon(data.weathercode)
       }
+    })
+    weatherApi = new WeatherAPI('46bbcf85e9a2e83913ce8271ffc4a79d');
+    weatherApi.getWeatherAPIContext().setCityId(cityId);
+    weatherApi.getWeatherAPIContext().getWeatherForecast(function (err, obj) {
+      console.log(obj)
     })
   },
   methods: {
@@ -50,12 +87,19 @@ export default {
 .weather {
   background: rgba(112, 168, 151, 0.81);
   padding: 1.8em;
+  border-radius: 1.3em;
 }
 
 .main-weather {
   font-size: 3.8em;
   font-weight: 600;
 }
+
+.main-weather-mini {
+  font-size: 3.8em;
+  font-weight: 600;
+}
+
 .main-weather-icon {
   color: #FFC8AA;
   padding-right: 0.3em;
@@ -64,5 +108,10 @@ export default {
 .temp {
 
   color: white;
+}
+
+.weather-description {
+  color: white;
+  font-size: .5em;
 }
 </style>
